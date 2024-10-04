b319e28417e6eb819a3191ef13c28bd4b967b6b3
EdwinSaucedoEncora <edwin.saucedo@encora.com>
2024-10-03 19:07:59 -0600

    feat: add regular expression code for python

--- Python/regular-expression-matching.py
+++ Python/regular-expression-matching.py
@@ -1,1 +1,10 @@
 
+import re
+
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        # Convert string p using regex because it applies the conditions provided
+        regex = re.compile(f'^{p}$')
+        # Get a boolean if exists a match with the string s
+        isMatch = True if regex.match(s) else False 
+        return isMatch
